355d93febee3c1e673b9c39e480d2e10a7fe2c33
Felipe Marcelino <fmarcelino@comcast.net>
2022-09-01 11:08:32 -0400

    Added license statement

--- macd.py
+++ macd.py
@@ -4,6 +4,22 @@
 Created on Tue Jun  7 13:00:00 2022
 
 @author: phil
+
+License Notice:
+This file is part of the Stock Analysis Toolset.
+
+The Stock Analysis Toolset is free software: you can redistribute it and/or 
+modify it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or (at your 
+option) any later version.
+
+Stock Analysis Toolset is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warrant of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+details.
+
+You should have received a copy of the GNU General Public License along with 
+the Stock Analysis Toolset.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import pandas as pd
